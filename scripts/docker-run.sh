573f26a2f2f21838e606510650d1452aba2b4df5
ShittyWizard <anisimov.develop@gmail.com>
2023-10-15 15:34:32 +0200

    Add Dockerfile and edit scripts for running docker containers (after building image with app);

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,1 +1,3 @@
-docker run --name wizard-space_postgres -e POSTGRES_USER=wizard -e POSTGRES_PASSWORD=54321 -d -p 5432:5432 postgres:alpine
+docker run --name wizard-space_postgres -e POSTGRES_USER=wizard -e POSTGRES_PASSWORD=54321 -d -p 5432:5432 --network wizard-space-infra postgres:alpine
+
+docker run --name wizard-space_app -p 8080:8080 --network wizard-space-infra wizard-space_app
--- scripts/docker-stop.sh
+++ scripts/docker-stop.sh
@@ -1,2 +1,5 @@
 docker stop wizard-space_postgres
 docker rm wizard-space_postgres
+
+docker stop wizard-space_app
+docker rm wizard-space_app
