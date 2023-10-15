910cbf69e63b01b1cbc56d4f22705b34537a60d3
ShittyWizard <anisimov.develop@gmail.com>
2023-10-15 14:20:16 +0200

    try to fix running tests;

--- src/main/java/app/record/repository/RecordRepository.java
+++ src/main/java/app/record/repository/RecordRepository.java
@@ -4,7 +4,6 @@ import app.record.model.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
